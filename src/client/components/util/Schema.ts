859e9f5a4796b9db922f067b4b4bf60fbb3e598f
jeremy-vondee <solomonodonkor79@gmail.com>
2024-02-07 17:57:58 +0000

    fix: Improved schema definitions

--- src/client/components/util/Schema.ts
+++ src/client/components/util/Schema.ts
@@ -1,101 +1,21 @@
 import { z } from "zod"
 
-const Schema = z
-    .object({
-        email: z.string().email({ message: "email is not valid" }),
-        password: z.string().min(8),
-        rememberPassword: z.boolean(),
-    })
-    // .superRefine(({ password }, checkPassComplexity) => {
-    //     const containsUppercase = (ch: string) => /[A-Z]/.test(ch)
-    //     const containsLowercase = (ch: string) => /[a-z]/.test(ch)
-    //     const containsSpecialChar = (ch: string) =>
-    //         /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(ch)
-    //     let countOfUpperCase = 0,
-    //         countOfLowerCase = 0,
-    //         countOfNumbers = 0,
-    //         countOfSpecialChar = 0
-
-    //     for (let i = 0; i < password.length; i++) {
-    //         let ch = password.charAt(i)
-    //         if (!isNaN(+ch)) countOfNumbers++
-    //         else if (containsUppercase(ch)) countOfUpperCase++
-    //         else if (containsLowercase(ch)) countOfLowerCase++
-    //         else if (containsSpecialChar(ch)) countOfSpecialChar++
-    //     }
-
-    //     let errObj = {
-    //         upperCase: { pass: true, message: "add upper case." },
-    //         lowerCase: { pass: true, message: "add lower case." },
-    //         specialCh: { pass: true, message: "add special ch." },
-    //         totalNumber: { pass: true, message: "add number." },
-    //     }
-
-    //     if (countOfLowerCase < 1) {
-    //         errObj = {
-    //             ...errObj,
-    //             lowerCase: { ...errObj.lowerCase, pass: false },
-    //         }
-    //     }
-    //     if (countOfNumbers < 1) {
-    //         errObj = {
-    //             ...errObj,
-    //             totalNumber: { ...errObj.totalNumber, pass: false },
-    //         }
-    //     }
-    //     if (countOfUpperCase < 1) {
-    //         errObj = {
-    //             ...errObj,
-    //             upperCase: { ...errObj.upperCase, pass: false },
-    //         }
-    //     }
-    //     if (countOfSpecialChar < 1) {
-    //         errObj = {
-    //             ...errObj,
-    //             specialCh: { ...errObj.specialCh, pass: false },
-    //         }
-    //     }
-
-    //     if (
-    //         countOfLowerCase < 1 ||
-    //         countOfUpperCase < 1 ||
-    //         countOfSpecialChar < 1 ||
-    //         countOfNumbers < 1
-    //     ) {
-    //         checkPassComplexity.addIssue({
-    //             code: "custom",
-    //             path: ["password"],
-    //             message: errObj,
-    //         })
-    //     }
-    // })
-    .superRefine(({ password }, checkPassComplexity) => {
-        const containsUppercase = (ch: string) => /[A-Z]/.test(ch)
-        const containsLowercase = (ch: string) => /[a-z]/.test(ch)
-        const containsSpecialChar = (ch: string) =>
-            /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(ch)
-        let countOfUpperCase = 0,
-            countOfLowerCase = 0,
-            countOfNumbers = 0,
-            countOfSpecialChar = 0
-        for (let i = 0; i < password.length; i++) {
-            let ch = password.charAt(i)
-            if (!isNaN(+ch)) countOfNumbers++
-            else if (containsUppercase(ch)) countOfUpperCase++
-            else if (containsLowercase(ch)) countOfLowerCase++
-            else if (containsSpecialChar(ch)) countOfSpecialChar++
-        }
-        if (
-            countOfLowerCase < 1 ||
-            countOfUpperCase < 1 ||
-            countOfSpecialChar < 1 ||
-            countOfNumbers < 1
-        ) {
-            checkPassComplexity.addIssue({
-                code: "custom",
-                message: "password does not meet complexity requirements",
-            })
-        }
-    })
+const Schema = z.object({
+    email: z.string().email({ message: "email is not valid" }),
+    password: z
+        .string()
+        .min(8, {
+            message: "Password must be more than 8 characters",
+        })
+        .max(32, {
+            message: "Password must not be more than 32 characters",
+        })
+        .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
+            message:
+                "Password must inculde 1 uppercase letter, 1lowercase letter, must contain 1 number and 1 special character",
+        }),
+    rememberPassword: z.boolean(),
+})
 
 export default Schema
+export type SchemaType = z.infer<typeof Schema>
