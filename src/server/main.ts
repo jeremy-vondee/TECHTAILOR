c4309ec697b0222ebef56902ed8e3e61569e7a87
jeremy-vondee <solomonodonkor79@gmail.com>
2024-02-15 18:33:48 +0000

    feat: Pushed product data to the front end

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,13 @@
-import express from "express";
-import ViteExpress from "vite-express";
+import express from "express"
+import ViteExpress from "vite-express"
+import Products from "./Products.json"
 
-const app = express();
+const app = express()
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
+app.get("/products", (req, res) => {
+    res.json(Products)
+})
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+    console.log("Server is listening on port 3000...")
+)
