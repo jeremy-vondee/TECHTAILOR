9ed530f6414a81eeeaa116b6dd5e839f185021a3
jeremy-vondee <solomonodonkor79@gmail.com>
2024-03-06 08:10:36 +0000

    perf: Dynamic routes destination implemented

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,17 +5,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import theme from "./components/layout/theme"
 //*Pages importation
-const AccessoriesPage = lazy(() => import("./components/pages/AccessoriesPage"))
-const ConsolesPage = lazy(() => import("./components/pages/ConsolesPage"))
 const ErrorPage = lazy(() => import("./components/pages/ErrorPage"))
 const App = lazy(() => import("./App"))
-const LaptopsPage = lazy(() => import("./components/pages/LaptopsPage"))
-const PhonesPage = lazy(() => import("./components/pages/PhonesPage"))
-const PrintersPage = lazy(() => import("./components/pages/PrintersPage"))
-const TvsPage = lazy(() => import("./components/pages/TvsPage"))
-const ToysPage = lazy(() => import("./components/pages/ToysPage"))
-const HardwaresPage = lazy(() => import("./components/pages/HardwaresPage"))
-const SecuritysPage = lazy(() => import("./components/pages/SecuritysPage"))
+const ProductsPage = lazy(() => import("./components/pages/ProductsPage"))
 const SignInPage = lazy(() => import("./components/pages/SignInPage"))
 const SignUpPage = lazy(() => import("./components/pages/SignUpPage"))
 
@@ -44,86 +36,15 @@ const router = createBrowserRouter([
         ),
     },
     {
-        path: "/category/phones",
+        path: "/categories/:category",
         element: (
             <Suspense>
                 {" "}
-                <PhonesPage />
+                <ProductsPage />
             </Suspense>
         ),
     },
-    {
-        path: "/category/tvs",
-        element: (
-            <Suspense>
-                {" "}
-                <TvsPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/printers",
-        element: (
-            <Suspense>
-                {" "}
-                <PrintersPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/laptops",
-        element: (
-            <Suspense>
-                {" "}
-                <LaptopsPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/toys",
-        element: (
-            <Suspense>
-                {" "}
-                <ToysPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/hardwares",
-        element: (
-            <Suspense>
-                {" "}
-                <HardwaresPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/securitys",
-        element: (
-            <Suspense>
-                {" "}
-                <SecuritysPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/consoles",
-        element: (
-            <Suspense>
-                {" "}
-                <ConsolesPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/category/accessories",
-        element: (
-            <Suspense>
-                {" "}
-                <AccessoriesPage />
-            </Suspense>
-        ),
-    },
+
     {
         path: "/*",
         element: (
