871379e36b5a108f8679b22645248137c3227ec3
jeremy-vondee <solomonodonkor79@gmail.com>
2024-03-12 12:50:13 +0000

    refactor: :recycle: Added zustand

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react"
 //* Layout importation
 import Footer from "./components/layout/Footer"
 import Header from "./components/layout/Header"
 import LandingPageBody from "./components/layout/LandingPageBody"
+//* Fetch util importation
+import { useProductStore } from "./components/store/useProductStore"
 
 function App() {
+    const { data, fetch } = useProductStore()
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
     return (
         <>
             <Header />
