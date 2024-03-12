45517c1c29c63991f32b58f52a33880f4620d712
jeremy-vondee <solomonodonkor79@gmail.com>
2024-03-12 12:52:40 +0000

    refactor: :recycle: Refactored to use Zustand state and added error page

--- src/client/components/pages/ProductPage.tsx
+++ src/client/components/pages/ProductPage.tsx
@@ -5,104 +5,117 @@ import { Stack, Grid, Box, Typography, Divider, Button } from "@mui/material"
 //* Layout importation
 import Header from "../layout/Header"
 import Footer from "../layout/Footer"
-import useFetch from "../hook/useFetch"
+//* ErrorPage importation
+import ErrorPage from "../pages/ErrorPage"
+//* Fetch util importation
+import { useProductStore } from "../store/useProductStore"
 
 const ProductPage: FC = () => {
     const { product } = useParams<string>()
-    const { fetchRes, error } = useFetch("/api/products")
+    const data = useProductStore((state) => state.data)
 
     return (
         <>
-            <Header />
-            {fetchRes !== null && fetchRes !== undefined
-                ? Object.values(fetchRes).map((productItem) =>
-                      Object.values(productItem).map((item) => {
-                          return item.name === product ? (
-                              <Grid
-                                  container
-                                  mt={{ xs: 14, sm: 18, md: 20 }}
-                                  mb={8}
-                                  key={item.name}>
-                                  <Grid item xs={12} sm={6} md={6}>
-                                      <Box
-                                          component="img"
-                                          alt={`${item.name} image`}
-                                          src={item.img}
-                                          sx={{
-                                              width: "100%",
-                                              height: "18rem",
-                                              objectFit: "contain",
-                                          }}
-                                      />
-                                  </Grid>
-                                  <Grid
-                                      item
-                                      xs={12}
-                                      sm={6}
-                                      md={6}
-                                      pl={{ xs: 3 }}
-                                      pr={{ xs: 3 }}>
-                                      <Typography
-                                          sx={{
-                                              fontWeight: "bolder",
-                                              fontSize: "2rem",
-                                          }}>
-                                          {item.name}
-                                      </Typography>
-                                      <Typography
-                                          sx={{
-                                              fontWeight: "bolder",
-                                              fontSize: "1.75rem",
-                                          }}>
-                                          {`GH₵ ` + item.price}
-                                      </Typography>
-                                      <Button
-                                          variant="contained"
-                                          disableElevation
-                                          sx={{
-                                              width: "fit-content",
-                                              marginTop: "8px",
-                                          }}>
-                                          add to cart
-                                      </Button>
-                                      <Divider sx={{ marginTop: "16px" }} />
-                                      <Stack>
-                                          <Typography mt={3}>
-                                              Description
-                                          </Typography>
-                                          <Stack
-                                              mt={1}
-                                              flexDirection={"row"}
-                                              alignItems={"center"}
-                                              sx={{ height: "2rem" }}>
-                                              <Typography
-                                                  variant="caption"
-                                                  sx={{
-                                                      fontWeight: "bold",
-                                                  }}>
-                                                  BRAND
-                                              </Typography>
-                                              <Typography
-                                                  pl={1}
-                                                  sx={{
-                                                      fontWeight: "bold",
-                                                  }}>
-                                                  {item.brand}
-                                              </Typography>
-                                          </Stack>
-                                          <Typography>
-                                              {item.fullSpecs}
-                                          </Typography>
-                                      </Stack>
-                                  </Grid>
-                              </Grid>
-                          ) : (
-                              ""
-                          )
-                      })
-                  )
-                : ""}
-            <Footer />
+            {data !== null &&
+            data !== undefined &&
+            Object.values(data).find((productDetail) =>
+                productDetail.find(
+                    (productName) => productName.name === product
+                )
+            ) ? (
+                <>
+                    <Header />
+                    {Object.values(data).map((productItem) =>
+                        Object.values(productItem).map((item) => {
+                            return item.name === product ? (
+                                <Grid
+                                    container
+                                    mt={{ xs: 14, sm: 18, md: 20 }}
+                                    mb={8}
+                                    key={item.name}>
+                                    <Grid item xs={12} sm={6} md={6}>
+                                        <Box
+                                            component="img"
+                                            alt={`${item.name} image`}
+                                            src={item.img}
+                                            sx={{
+                                                width: "100%",
+                                                height: "18rem",
+                                                objectFit: "contain",
+                                            }}
+                                        />
+                                    </Grid>
+                                    <Grid
+                                        item
+                                        xs={12}
+                                        sm={6}
+                                        md={6}
+                                        pl={{ xs: 3 }}
+                                        pr={{ xs: 3 }}>
+                                        <Typography
+                                            sx={{
+                                                fontWeight: "bolder",
+                                                fontSize: "2rem",
+                                            }}>
+                                            {item.name}
+                                        </Typography>
+                                        <Typography
+                                            sx={{
+                                                fontWeight: "bolder",
+                                                fontSize: "1.75rem",
+                                            }}>
+                                            {`GH₵ ` + item.price}
+                                        </Typography>
+                                        <Button
+                                            variant="contained"
+                                            disableElevation
+                                            sx={{
+                                                width: "fit-content",
+                                                marginTop: "8px",
+                                            }}>
+                                            add to cart
+                                        </Button>
+                                        <Divider sx={{ marginTop: "16px" }} />
+                                        <Stack>
+                                            <Typography mt={3}>
+                                                Description
+                                            </Typography>
+                                            <Stack
+                                                mt={1}
+                                                flexDirection={"row"}
+                                                alignItems={"center"}
+                                                sx={{ height: "2rem" }}>
+                                                <Typography
+                                                    variant="caption"
+                                                    sx={{
+                                                        fontWeight: "bold",
+                                                    }}>
+                                                    BRAND
+                                                </Typography>
+                                                <Typography
+                                                    pl={1}
+                                                    sx={{
+                                                        fontWeight: "bold",
+                                                    }}>
+                                                    {item.brand}
+                                                </Typography>
+                                            </Stack>
+                                            <Typography>
+                                                {item.fullSpecs}
+                                            </Typography>
+                                        </Stack>
+                                    </Grid>
+                                </Grid>
+                            ) : (
+                                ""
+                            )
+                        })
+                    )}
+                    <Footer />
+                </>
+            ) : (
+                <ErrorPage />
+            )}
         </>
     )
 }
