9578519ccdf5a8ae4dafbdf3fa0edf111b922318
jeremy-vondee <solomonodonkor79@gmail.com>
2024-05-18 09:56:01 +0000

    feat: :sparkles: Local storage added to persist items added to the cart

--- src/client/components/store/useAddToCartStore.ts
+++ src/client/components/store/useAddToCartStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand"
 import { productDataType } from "./useProductStore"
+import { persist } from "zustand/middleware"
 
 export type cartType = {
     cartItems: productDataType[]
@@ -8,54 +9,68 @@ export type cartType = {
     decreaseQuantity: (item: productDataType) => void
 }
 
-export const useAddToCartStore = create<cartType>((set, get) => ({
-    cartItems: [],
+export const useAddToCartStore = create(
+    persist<cartType>(
+        (set, get) => ({
+            cartItems: [],
 
-    addItemToCart: (item) => {
-        const itemExist = get().cartItems.find(
-            (cartItem) => cartItem.name === item.name
-        )
+            addItemToCart: (item) => {
+                const itemExist = get().cartItems.find(
+                    (cartItem) => cartItem.name === item.name
+                )
 
-        if (itemExist) {
-            if (typeof itemExist.quantity === "number") {
-                itemExist.quantity++
-            }
+                if (itemExist) {
+                    if (typeof itemExist.quantity === "number") {
+                        itemExist.quantity++
+                    }
 
-            set({ cartItems: [...get().cartItems] })
-        } else {
-            set({ cartItems: [...get().cartItems, { ...item, quantity: 1 }] })
-        }
-    },
-
-    increaseQuantity: (item) => {
-        const items = get().cartItems
-        const itemExist = items.find((cartItem) => cartItem.name === item.name)
-
-        if (itemExist) {
-            if (typeof itemExist.quantity === "number") {
-                itemExist.quantity++
-            }
-
-            set({ cartItems: [...items] })
-        }
-    },
-
-    decreaseQuantity: (item) => {
-        const items = get().cartItems
-        const itemExist = items.find((cartItem) => cartItem.name === item.name)
-
-        if (itemExist) {
-            if (typeof itemExist.quantity === "number") {
-                if (itemExist.quantity <= 1) {
-                    const updatedCartItems = items.filter(
-                        (item) => item.name !== item.name
-                    )
-                    set({ cartItems: updatedCartItems })
+                    set({ cartItems: [...get().cartItems] })
                 } else {
-                    itemExist.quantity--
+                    set({
+                        cartItems: [
+                            ...get().cartItems,
+                            { ...item, quantity: 1 },
+                        ],
+                    })
+                }
+            },
+
+            increaseQuantity: (item) => {
+                const items = get().cartItems
+                const itemExist = items.find(
+                    (cartItem) => cartItem.name === item.name
+                )
+
+                if (itemExist) {
+                    if (typeof itemExist.quantity === "number") {
+                        itemExist.quantity++
+                    }
+
                     set({ cartItems: [...items] })
                 }
-            }
-        }
-    },
-}))
+            },
+
+            decreaseQuantity: (item) => {
+                const items = get().cartItems
+                const itemExist = items.find(
+                    (cartItem) => cartItem.name === item.name
+                )
+
+                if (itemExist) {
+                    if (typeof itemExist.quantity === "number") {
+                        if (itemExist.quantity <= 1) {
+                            const updatedCartItems = items.filter(
+                                (item) => item.name !== item.name
+                            )
+                            set({ cartItems: updatedCartItems })
+                        } else {
+                            itemExist.quantity--
+                            set({ cartItems: [...items] })
+                        }
+                    }
+                }
+            },
+        }),
+        { name: "cart-items" }
+    )
+)
