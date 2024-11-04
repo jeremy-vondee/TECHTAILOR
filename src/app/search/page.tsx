d804386560c3db017b8910e629887ed6c763657b
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-04 12:07:05 +0000

    feat: :sparkles: Search page added

---
+++ src/app/search/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from "react"
+import { Stack, Typography } from "@mui/material"
+import Header from "../components/header/page"
+import { useSearchParams } from "next/navigation"
+
+const Search = () => {
+  const searchQuery = useSearchParams().get("q") as string
+
+  return (
+    <>
+      <Header />
+      <Stack mt={15}>
+        {searchQuery && (
+          <Typography variant="body1" sx={{ mt: 2 }}>
+            Showing results for: {searchQuery}
+          </Typography>
+        )}
+      </Stack>
+    </>
+  )
+}
+
+export default Search
