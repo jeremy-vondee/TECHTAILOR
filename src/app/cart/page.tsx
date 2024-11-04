7ea24c948a160b46f4cee51fbc21f197d3f80929
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-04 12:05:40 +0000

    feat: :sparkles: Shopping cart funtionality added

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+const Cart = () => {
+  return (
+    <>
+      <Typography>Cart</Typography>
+    </>
+  )
+}
+
+export default Cart
