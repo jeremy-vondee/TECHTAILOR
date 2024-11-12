b9730b4af609a1c1740bf14a6e50dd1d38a642c7
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-12 14:48:08 +0000

    feat: :sparkles: Homepage feature added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import React from "react"
-import Header from "./components/header/page"
+"use client"
+import React, { useEffect } from "react"
+import Header from "./_components/header/page"
+import { useProductStore } from "@/store/productSotre"
+import FeaturedProducts from "./_components/featuredProducts/page"
+import Footer from "./_components/footer/page"
 
 const Home = () => {
-  return <Header />
+  const { isLoading, error, fetchData } = useProductStore()
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+
+  return (
+    <>
+      <Header />
+      <FeaturedProducts featuredCategory="laptops" />
+      <FeaturedProducts featuredCategory="phones" />
+      <FeaturedProducts featuredCategory="consoles" />
+      <Footer />
+    </>
+  )
 }
 
 export default Home
