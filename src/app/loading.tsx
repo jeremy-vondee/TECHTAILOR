569956a67ff1b8a05eb9f9f7ed140d9c85e493bf
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-23 13:07:23 +0000

    feat: :sparkles: Home page loading skeleton added

---
+++ src/app/loading.tsx
@@ -0,0 +1,450 @@
+import React from "react"
+import { Grid2, Skeleton, Stack } from "@mui/material"
+
+const LoadingSkeleton = () => {
+  return (
+    <>
+      <Stack>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            height: { xs: 120, sm: 130 }
+          }}
+        />
+        <Grid2
+          container
+          mt={5}
+          ml={3}
+          gap={{ xs: 3, sm: 5, md: 3 }}
+          columnGap={{ xs: 4, sm: 5 }}
+        >
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+        </Grid2>
+      </Stack>
+      <Stack mt={5}>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            height: { xs: "10vh", sm: "12vh" }
+          }}
+        />
+        <Grid2
+          container
+          mt={5}
+          ml={3}
+          gap={{ xs: 3, sm: 5, md: 3 }}
+          columnGap={{ xs: 4, sm: 5 }}
+        >
+          <Grid2 size={{ xs: 5, sm: 3.6, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "55%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+        </Grid2>
+      </Stack>
+      <Stack mt={5}>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            height: { xs: "10vh", sm: "12vh" }
+          }}
+        />
+        <Grid2
+          container
+          mt={5}
+          ml={3}
+          gap={{ xs: 3, sm: 5, md: 3 }}
+          columnGap={{ xs: 4, sm: 5 }}
+        >
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+        </Grid2>
+      </Stack>
+      <Stack mt={5}>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            height: { xs: "10vh", sm: "12vh" }
+          }}
+        />
+        <Grid2
+          container
+          mt={5}
+          ml={3}
+          gap={{ xs: 3, sm: 5, md: 3 }}
+          columnGap={{ xs: 4, sm: 5 }}
+        >
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+          <Grid2 size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+        </Grid2>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            height: { xs: 130, sm: 150 },
+            marginTop: "40px"
+          }}
+        />
+      </Stack>
+    </>
+  )
+}
+
+export default LoadingSkeleton
