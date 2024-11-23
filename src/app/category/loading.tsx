00d192abd045f1025c23d96575f78e86ddb98eab
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-23 13:13:56 +0000

    feat: :sparkles: Products skeleton added

---
+++ src/app/category/loading.tsx
@@ -0,0 +1,61 @@
+import React from "react"
+import { Grid2, Skeleton, Stack } from "@mui/material"
+
+interface ProductsSkeletonProp {
+  count: number
+}
+
+const ProductsSkeleton = ({ count }: ProductsSkeletonProp) => {
+  return (
+    <>
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          height: { xs: 120, sm: 130 }
+        }}
+      />
+      <Grid2
+        container
+        mt={5}
+        ml={3}
+        gap={{ xs: 3, sm: 5, md: 3 }}
+        columnGap={{ xs: 4, sm: 5 }}
+      >
+        {Array.from({ length: count }).flatMap(() => (
+          <Grid2 key={count} size={{ xs: 5, sm: 3, md: 2.5 }}>
+            <Stack>
+              <Skeleton
+                variant="rounded"
+                sx={{ height: { xs: 120, md: 160 } }}
+              />
+              <Skeleton
+                variant="text"
+                height={30}
+                sx={{ width: { xs: "80%", md: "70%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "60%", sm: "55%", md: "48%" } }}
+              />
+              <Skeleton
+                variant="text"
+                height={15}
+                sx={{ width: { xs: "40%", md: "35%" } }}
+              />
+            </Stack>
+          </Grid2>
+        ))}
+      </Grid2>
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          height: { xs: 130, sm: 150 },
+          marginTop: "40px"
+        }}
+      />
+    </>
+  )
+}
+
+export default ProductsSkeleton
