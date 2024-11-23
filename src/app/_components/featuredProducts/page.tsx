985dbcfcf9677c55e1ddc533ef1212bd3203bafb
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-23 13:05:19 +0000

    fix: :iphone: Moblie design improved and Grid componet renamed

--- src/app/_components/featuredProducts/page.tsx
+++ src/app/_components/featuredProducts/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import React from "react"
 import { useProductStore } from "@/store/productSotre"
-import Grid from "@mui/material/Grid2"
-import { Box, Link, Stack, Typography, useTheme } from "@mui/material"
+import { Box, Grid2, Link, Stack, Typography, useTheme } from "@mui/material"
 import NextLink from "next/link"
+import StarRateIcon from "@mui/icons-material/StarRate"
 
 const FeaturedProducts = ({
   featuredCategory
@@ -12,15 +12,14 @@ const FeaturedProducts = ({
 }) => {
   const { products } = useProductStore()
   const theme = useTheme()
-
   return (
     <>
-      <Stack mt={20}>
+      <Stack mt={5}>
         <Stack
           justifyContent={"center"}
           sx={{
             backgroundColor: theme.palette.primary.main,
-            height: "12vh",
+            height: { xs: "10vh", sm: "12vh" },
             verticalAlign: "middle"
           }}
         >
@@ -35,10 +34,10 @@ const FeaturedProducts = ({
             {featuredCategory.toUpperCase()}
           </Typography>
         </Stack>
-        <Grid
+        <Grid2
           container
           pl={4}
-          mt={4}
+          mt={5}
           gap={{ xs: 3, sm: 5, md: 3 }}
           columnGap={{ xs: 4, sm: 5 }}
         >
@@ -49,7 +48,7 @@ const FeaturedProducts = ({
                   Object.entries(categoryProducts ?? {})
                     .slice(0, 4)
                     .map(([, product]) => (
-                      <Grid
+                      <Grid2
                         key={product.name}
                         size={{ xs: 5, sm: 4, md: 3 }}
                         flexDirection={"row"}
@@ -82,30 +81,56 @@ const FeaturedProducts = ({
                           />
                           <Stack>
                             <Typography
+                              variant="body2"
                               sx={{
-                                fontSize: { xs: "0.75rem", md: "1.1rem" },
                                 fontWeight: 590,
                                 width: { xs: "100%" }
                               }}
                             >
                               {product.name}
                             </Typography>
-
-                            <Typography
-                              variant="caption"
-                              sx={{
-                                fontSize: { xs: "0.75rem", md: "1.1rem" }
-                              }}
-                            >
-                              {`£${product.price}`}
-                            </Typography>
+                            <Stack flexDirection={"row"} gap={1}>
+                              <Typography
+                                variant="caption"
+                                sx={{
+                                  fontSize: "0.75rem"
+                                }}
+                              >
+                                {`£${product.price}`}
+                              </Typography>
+                              <Typography
+                                variant="caption"
+                                sx={{
+                                  fontSize: "0.75rem",
+                                  color: "#aaa",
+                                  textDecoration: "line-through"
+                                }}
+                              >
+                                {`£${product.oldPrice}`}
+                              </Typography>
+                            </Stack>
+                          </Stack>
+                          <Stack direction="row">
+                            {[...Array(5)].map((_, index) => (
+                              <StarRateIcon
+                                key={index}
+                                sx={{
+                                  fontSize: { xs: "0.75rem", md: "0.85rem" }
+                                }}
+                                color={
+                                  index < Math.floor(product.rating)
+                                    ? "primary"
+                                    : "disabled"
+                                }
+                              />
+                            ))}
                           </Stack>
                         </Link>
-                      </Grid>
+                      </Grid2>
                     ))
                 )
             : "No product found"}
-        </Grid>
+        </Grid2>
       </Stack>
     </>
   )
