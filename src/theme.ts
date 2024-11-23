77e992e05bb60ff923680f788bc09b2c4ae7eb9f
jeremy-vondee <solomonodonkor79@gmail.com>
2024-11-23 12:44:07 +0000

    feat: :wrench: OpenSans semi-bold font added

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,10 @@ const openSans = localFont({
   src: "./app/font/OpenSans-Regular.ttf",
   display: "swap"
 })
+const openSansBold = localFont({
+  src: "./app/font/OpenSans-Semibold.ttf",
+  display: "swap"
+})
 
 const theme = createTheme({
   palette: {
@@ -17,7 +21,15 @@ const theme = createTheme({
     }
   },
   typography: {
-    fontFamily: openSans.style.fontFamily
+    fontFamily: [openSans.style.fontFamily, openSansBold.style.fontFamily].join(
+      ","
+    ),
+    body1: {
+      fontFamily: openSans.style.fontFamily
+    },
+    body2: {
+      fontFamily: openSansBold.style.fontFamily
+    }
   }
 })
 
